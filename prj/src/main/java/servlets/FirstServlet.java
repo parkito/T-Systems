fc6201f3a1d923038d4e2fa31dac3868ae39c3e9
parkito <artyom-karnov@yandex.ru>
2016-08-16 18:31:20 +0300

    new files

---
+++ prj/src/main/java/servlets/FirstServlet.java
@@ -0,0 +1,27 @@
+package servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by Artyom Karnov on 8/16/16.
+ * artyom-karnov@yandex.ru
+ **/
+@WebServlet(name = "FirstServlet")
+public class FirstServlet extends HttpServlet {
+    @Override
+    public void init() throws ServletException {
+        System.out.println("test");
+        super.init();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().write("hi");
+        System.out.println("it works");
+    }
+}
