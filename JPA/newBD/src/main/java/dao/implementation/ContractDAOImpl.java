b0f75f971e308b68fbcc2723c145b9b9f6dac198
parkito <artyom-karnov@yandex.ru>
2016-08-26 21:48:37 +0300

    newBase5

--- JPA/newBD/src/main/java/dao/implementation/ContractDAOImpl.java
+++ JPA/newBD/src/main/java/dao/implementation/ContractDAOImpl.java
@@ -4,5 +4,47 @@ package dao.implementation;
  * Created by Artyom Karnov on 8/26/16.
  * artyom-karnov@yandex.ru
  **/
-public class ContractDAOImpl {
+
+
+import dao.api.ContractDAO;
+import entities.Contract;
+import exceptions.ContractNotFoundException;
+import exceptions.ContractsForEntityNotGotException;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceException;
+import javax.persistence.Query;
+import java.util.List;
+
+/**
+ * Created by Artyom Karnov on 8/26/16.
+ * artyom-karnov@yandex.ru
+ **/
+
+public class ContractDAOImpl extends GenericDAOImpl<Contract, Integer> implements ContractDAO {
+    @PersistenceContext
+    private EntityManager entityManager;
+
+
+    @Override
+    public Contract getContractByNumber(String number) throws ContractNotFoundException {
+        try {
+            return (Contract) entityManager.createQuery("select c from Contract c where c.number=:number").setParameter("number", number).getSingleResult();
+        } catch (PersistenceException e) {
+            throw new ContractNotFoundException("Contract " + number + " wasn't gotten", e);
+        }
+    }
+
+    @Override
+    public List<Contract> getAllUserContracts(int id) throws ContractsForEntityNotGotException {
+        try {
+            Query query = entityManager.createQuery("select u.contracts from User u where u.id=:id").setParameter("id", id);
+
+            return (List<Contract>) query.getResultList();
+        } catch (PersistenceException ex) {
+            throw new ContractsForEntityNotGotException("Contracts for user " + id + " wasn't gotten", ex);
+        }
+    }
+
 }
--- JPA/newBD/src/main/java/dao/implementation/GenericDAOImpl.java
+++ JPA/newBD/src/main/java/dao/implementation/GenericDAOImpl.java
@@ -1,8 +1,77 @@
 package dao.implementation;
 
+import dao.api.GenericDAO;
+import exceptions.CustomDAOException;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceException;
+import java.lang.reflect.ParameterizedType;
+import java.util.List;
+
 /**
  * Created by Artyom Karnov on 8/26/16.
  * artyom-karnov@yandex.ru
  **/
-public class GenericDAOImpl {
+public abstract class GenericDAOImpl<E, K> implements GenericDAO<E, K> {
+    protected Class<E> daoType;
+
+    @SuppressWarnings("unchecked")
+    public GenericDAOImpl() {
+        daoType = (Class<E>) ((ParameterizedType) getClass().getGenericSuperclass())
+                .getActualTypeArguments()[0];
+    }
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Override
+    public void create(E entity) throws CustomDAOException {
+        try {
+            this.entityManager.persist(entity);
+        } catch (PersistenceException e) {
+            throw new CustomDAOException("Entity wasn't created: " + entity, e);
+        }
+    }
+
+    @Override
+    public E read(K id) throws CustomDAOException {
+        try {
+            return (E) this.entityManager.find(daoType, id);
+        } catch (PersistenceException e) {
+            throw new CustomDAOException("Entity " + id + " wasn't found", e);
+        }
+    }
+
+    @Override
+    public void update(E entity) throws CustomDAOException {
+        try {
+            this.entityManager.merge(entity);
+        } catch (PersistenceException e) {
+            throw new CustomDAOException("Entity wasn't updated: " + entity, e);
+        }
+
+    }
+
+    @Override
+    public void delete(E entity) throws CustomDAOException {
+        try {
+            this.entityManager.remove(entityManager.merge(entity));
+        } catch (PersistenceException e) {
+            throw new CustomDAOException("Entity wasn't deleted: " + entity, e);
+        }
+
+    }
+
+    @Override
+    public List<E> getAll() throws CustomDAOException {
+        try {
+            return this.entityManager.createNamedQuery(daoType.getSimpleName() + ".getAll", daoType).getResultList();
+        } catch (PersistenceException ex) {
+            throw new CustomDAOException("Unable to get all entities of class " + daoType.getSimpleName(), ex);
+        }
+    }
+
+
 }
+
