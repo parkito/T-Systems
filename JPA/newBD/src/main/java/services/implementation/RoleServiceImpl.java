e82ad94d38948ffd215833052dedeed36b7762d7
parkito <artyom-karnov@yandex.ru>
2016-08-27 03:02:51 +0300

    HW4

---
+++ JPA/newBD/src/main/java/services/implementation/RoleServiceImpl.java
@@ -0,0 +1,47 @@
+package services.implementation;
+
+
+import dao.api.AccessLevelDAO;
+import entities.AccessLevel;
+import exceptions.CustomDAOException;
+import services.api.AccessLevelService;
+
+import java.util.List;
+
+/**
+ * Created by Artyom Karnov on 8/27/16.
+ * artyom-karnov@yandex.ru
+ **/
+
+public class RoleServiceImpl implements AccessLevelService {
+    private AccessLevelDAO accessLevelDAO;
+
+    @Override
+
+    public void createEntity(AccessLevel accessLevel) throws CustomDAOException {
+        this.accessLevelDAO.create(accessLevel);
+    }
+
+    @Override
+    public AccessLevel getEntityById(Integer id) throws CustomDAOException {
+        return this.accessLevelDAO.read(id);
+    }
+
+    @Override
+    public void updateEntity(AccessLevel accessLevel) throws CustomDAOException {
+        this.accessLevelDAO.update(accessLevel);
+    }
+
+    @Override
+    public void deleteEntity(AccessLevel accessLevel) throws CustomDAOException {
+        this.accessLevelDAO.delete(accessLevel);
+
+    }
+
+    @Override
+    public List<AccessLevel> getAll() throws CustomDAOException {
+        return this.accessLevelDAO.getAll();
+    }
+}
+
+
