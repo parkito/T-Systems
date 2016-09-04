9aab563c1fd5c00955711e8d711120eb111f51d9
parkito <artyom-karnov@yandex.ru>
2016-09-04 20:05:09 +0300

    first M#8

--- JPA/newBD/src/main/java/controllers/manager/FindClientServlet.java
+++ JPA/newBD/src/main/java/controllers/manager/FindClientServlet.java
@@ -1,6 +1,7 @@
 package controllers.manager;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -10,13 +11,17 @@ import java.io.IOException;
  * Created by Artyom Karnov on 9/4/16.
  * artyom-karnov@yandex.ru
  **/
+@WebServlet(name = "FindClientServlet", urlPatterns = {"/manager/FindClient"})
 public class FindClientServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("HI");
         req.getRequestDispatcher("/WEB-INF/manager/FindClient.jsp").forward(req, resp);
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("HI");
+
     }
 }
--- JPA/newBD/src/main/java/controllers/manager/NewClientServlet.java
+++ JPA/newBD/src/main/java/controllers/manager/NewClientServlet.java
@@ -1,8 +1,23 @@
 package controllers.manager;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 /**
  * Created by Artyom Karnov on 9/4/16.
  * artyom-karnov@yandex.ru
  **/
-public class NewClientServlet {
+public class NewClientServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("Get");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("Post");
+    }
 }
