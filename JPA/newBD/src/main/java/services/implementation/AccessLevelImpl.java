fb187fd4d0740e40de47737d75740d55b4e55e71
parkito <artyom-karnov@yandex.ru>
2016-09-12 02:18:56 +0300

    Work on mistakes  #2.3

--- JPA/newBD/src/main/java/services/implementation/AccessLevelImpl.java
+++ JPA/newBD/src/main/java/services/implementation/AccessLevelImpl.java
@@ -14,6 +14,7 @@ import java.util.List;
  * artyom-karnov@yandex.ru
  **/
 
+
 public class AccessLevelImpl implements AccessLevelService {
     private AccessLevelDAO accessLevelDAO = new AccessLevelDAOImpl();
 
