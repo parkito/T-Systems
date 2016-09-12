d1c6ae76f4faa68fe05c756fbe00471f383ecc91
parkito <artyom-karnov@yandex.ru>
2016-09-12 22:13:10 +0300

    Work on mistakes  #3.2

--- JPA/newBD/src/main/java/controllers/UserCases.java
+++ JPA/newBD/src/main/java/controllers/UserCases.java
@@ -1,8 +1,72 @@
 package controllers;
 
+
+import entities.Contract;
+import entities.User;
+import exceptions.UserNotFoundException;
+import services.implementation.AccessLevelImpl;
+import services.implementation.ContractServiceImpl;
+import services.implementation.UserServiceImpl;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
 /**
- * Created by Artyom Karnov on 9/12/16.
+ * Created by Artyom Karnov on 8/30/16.
  * artyom-karnov@yandex.ru
  **/
 public class UserCases {
+    private UserServiceImpl userService = new UserServiceImpl();
+    private AccessLevelImpl accessLevelService = new AccessLevelImpl();
+    private ContractServiceImpl contractService = new ContractServiceImpl();
+
+    /**
+     *
+     * @param user
+     */
+    public void makeUserManager(User user) {
+        userService.cahngeUserAccessLevel(user, accessLevelService.getEntityById(3));
+    }
+
+    /**
+     *
+     * @param contract
+     * @return
+     */
+    public String getUserName(Contract contract) {
+        return contract.getUser().getName();
+
+    }
+
+    /**
+     *
+     * @param eMail
+     * @return
+     */
+    public boolean isManager(String eMail) {
+        return userService.getUserByEMAil(eMail).getAccessLevel() != null ? true : false;
+    }
+
+    /**
+     *
+     * @param eMail
+     * @return
+     */
+    public String getUserNameByEmail(String eMail) {
+        return userService.getUserByEMAil(eMail).getName();
+    }
+
+    /**
+     *
+     * @param eMail
+     * @return
+     */
+    public List<Contract> getAllContractsForUser(String eMail) {
+        userService.updateEntity(userService.getUserByEMAil(eMail));
+        return contractService.getAllContractsForUser(userService.getUserByEMAil(eMail).getUserId());
+
+    }
+
+
 }
---
+++ JPA/newBD/src/tests/unit/controllers/UserCasesTest.java
@@ -0,0 +1,37 @@
+package unit.controllers;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Artyom Karnov on 9/12/16.
+ * artyom-karnov@yandex.ru
+ **/
+public class UserCasesTest {
+    @Test
+    public void makeUserManager() throws Exception {
+
+    }
+
+    @Test
+    public void getUserName() throws Exception {
+
+    }
+
+    @Test
+    public void isManager() throws Exception {
+
+    }
+
+    @Test
+    public void getUserNameByEmail() throws Exception {
+
+    }
+
+    @Test
+    public void getAllContractsForUser() throws Exception {
+
+    }
+
+}
