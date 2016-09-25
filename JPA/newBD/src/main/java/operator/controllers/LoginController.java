dda292d81747d256848724743b7db942792b931d
parkito <artyom-karnov@yandex.ru>
2016-09-25 18:17:19 +0300

    with SS#1

--- JPA/newBD/src/main/java/operator/controllers/LoginController.java
+++ JPA/newBD/src/main/java/operator/controllers/LoginController.java
@@ -1,14 +1,17 @@
 package operator.controllers;
 
+import operator.entities.User;
 import operator.services.api.UserService;
-import operator.services.implementation.UserServiceImpl;
+import operator.utils.Locale.RussianLanguage;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.Locale;
 
 /**
@@ -22,6 +25,7 @@ public class LoginController {
     @Autowired
     private UserService userService;
 
+
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String home(Locale locale, Model model) {
 
@@ -32,5 +36,66 @@ public class LoginController {
     public String login(@RequestParam("username") String eMail, @RequestParam("password") String pass) {
         System.out.println(userService.getUserByEMAil(eMail));
         return "index";
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String loginPage(Locale locale, Model model) {
+        return "login";
+    }
+
+    /**
+     * '
+     * This method returns the login page when logout is required.
+     *
+     * @return login.jsp
+     */
+    @RequestMapping(value = "/logout", method = RequestMethod.GET)
+    public String logoutPage() {
+        return "index";
+    }
+
+    /**
+     * This method returns an error403 page when a restricted access page is called.
+     *
+     * @return denied.jsp
+     */
+    @RequestMapping(value = "/denied", method = RequestMethod.GET)
+    public String deniedPage() {
+        return "404";
+    }
+
+    /**
+     * This method returns a login page with an error block after an unsuccessful attempt.
+     *
+     * @param locale locale;
+     * @param model  model;
+     * @return login.jsp
+     */
+    @RequestMapping(value = "/login-denied", method = RequestMethod.GET)
+    public String loginDenied(Locale locale, Model model) {
+        model.addAttribute("isInputValid", "false");
+        return "index";
+    }
+
+    /**
+     * This method dispatches the requests to the starting page of an employee or to the one of a user.
+     * It also sets the current user entity into session.
+     *
+     * @param request request;
+     * @param locale  locale;
+     * @param model   model;
+     * @return cp_employee_main.jsp or cp_client_main.jsp
+     */
+    @RequestMapping(value = "/main", method = RequestMethod.GET)
+    public String dispatch(HttpServletRequest request, Locale locale, Model model) {
+        org.springframework.security.core.userdetails.User user = (org.springframework.security.core.userdetails.User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        User currentUser = userService.getUserByEMAil(user.getUsername());
+        request.getSession().setAttribute("currentUserU", currentUser);
+        request.getSession().setAttribute("language", RussianLanguage.getRussianLanguage());
+        if (currentUser.getAccessLevel() == null) {
+            return "user/index";
+        } else  {
+            return "admin/index";
+        }
     }
 }
