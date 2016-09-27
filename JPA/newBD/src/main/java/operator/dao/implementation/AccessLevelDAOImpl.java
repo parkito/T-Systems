67acea57f95a0aa3b5afba73aea166071bfb7bb4
parkito <artyom-karnov@yandex.ru>
2016-09-28 02:37:18 +0300

    Slowly go #17

--- JPA/newBD/src/main/java/operator/dao/implementation/AccessLevelDAOImpl.java
+++ JPA/newBD/src/main/java/operator/dao/implementation/AccessLevelDAOImpl.java
@@ -4,6 +4,9 @@ import operator.dao.api.AccessLevelDAO;
 import operator.entities.AccessLevel;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 /**
  * Created by Artyom Karnov on 8/26/16.
  * artyom-karnov@yandex.ru
@@ -14,5 +17,7 @@ import org.springframework.stereotype.Repository;
  */
 @Repository("AccessLevel")
 public class AccessLevelDAOImpl extends GenericDAOImpl<AccessLevel, Integer> implements AccessLevelDAO {
+    @PersistenceContext
+    private EntityManager entityManager;
 }
 
