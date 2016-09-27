b72228923d7f7e4beca03b683510c3ccf50bce56
parkito <artyom-karnov@yandex.ru>
2016-09-28 01:25:30 +0300

    Slowly go #16

---
+++ JPA/newBD/src/main/java/operator/configuration/SecurityWebApplicationInitializer.java
@@ -0,0 +1,7 @@
+package operator.configuration;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
