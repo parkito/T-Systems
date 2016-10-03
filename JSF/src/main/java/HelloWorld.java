72ac8378b3a8be1f8f21d7aed8f92956e2f98c1a
parkito <artyom-karnov@yandex.ru>
2016-10-03 22:55:40 +0300

    Slowly go #33.1.1

---
+++ JSF/src/main/java/HelloWorld.java
@@ -0,0 +1,27 @@
+/**
+ * Created by Artyom Karnov on 10/3/16.
+ * artyom-karnov@yandex.ru
+ **/
+import java.io.Serializable;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+
+@ManagedBean(name="helloWorld")
+@SessionScoped
+public class HelloWorld implements Serializable{
+
+    private static final long serialVersionUID = -6913972022251814607L;
+
+    private String s1 = "Hello World!!";
+
+    public String getS1() {
+        System.out.println(s1);
+        return s1;
+    }
+
+    public void setS1(String s1) {
+        this.s1 = s1;
+    }
+
+}
