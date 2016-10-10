d58a7a7d69b218aedc5c2aff948d1bfd178aa731
parkito <artyom-karnov@yandex.ru>
2016-10-10 15:34:26 +0300

    Slowly go #33.8.7

--- eCare/OperatorMainApp/src/tests/unit/controllers/ManagerCasesTest.java
+++ eCare/OperatorMainApp/src/tests/unit/controllers/ManagerCasesTest.java
@@ -1,13 +1,19 @@
 package unit.controllers;
 
 import operator.controllers.ManagerCases;
+import operator.entities.Contract;
+import operator.entities.Tariff;
 import operator.exceptions.CustomDAOException;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.when;
@@ -21,6 +27,10 @@ public class ManagerCasesTest {
     @Mock
     ManagerCases managerCasesMock;
 
+    private List<Contract> contracts = new ArrayList<>();
+    private List<Contract> spyContract = Mockito.spy(contracts);
+
+
     @Before
     public void beforeTest() {
         when(managerCasesMock.isUserExists("c@b.ru")).thenReturn(false);
@@ -30,9 +40,9 @@ public class ManagerCasesTest {
         when(managerCasesMock.isNumberExists("214189")).thenReturn(true);
         when(managerCasesMock.isTariffExists("call1")).thenReturn(true);
         doThrow(new CustomDAOException("Tariff already exists"))
-                .when(managerCasesMock).addContractToBase("base", "100.0");
+                .when(managerCasesMock).addContractToBase("c@b.ru", "214189");
         doThrow(new CustomDAOException("Contract already exists"))
-                .when(managerCasesMock).addTariffToBase("c@b.ru", "214189");
+                .when(managerCasesMock).addTariffToBase("base", "100");
         when(managerCasesMock.isOptionExists("base")).thenReturn(true);
         doThrow(new CustomDAOException("Contract already exists"))
                 .when(managerCasesMock).addOptionToBase("new", "100", "100");
@@ -55,6 +65,28 @@ public class ManagerCasesTest {
             managerCasesMock.addUserToBase("art", "kar", "08.02.1995",
                     "pass", "addr", "c@b.ru", "pass");
         }
+    }
+
+    @Test(expected = CustomDAOException.class)
+    public void newTariffToBaseError() {
+        if (!managerCasesMock.isTariffExists("base")) {
+            managerCasesMock.addTariffToBase("base", "100");
+        }
+    }
+
+    @Test
+    public void newTariffToBase() {
+        if (!managerCasesMock.isTariffExists("base")) {
+            managerCasesMock.addTariffToBase("calls", "100");
+        }
+    }
+
+    @Test
+    public void addContractToList() {
+        spyContract.add(new Contract("214189", new Tariff()));
+        spyContract.add(new Contract("214190", new Tariff()));
+        spyContract.add(new Contract("214191", new Tariff()));
+        Assert.assertEquals(spyContract.get(0).getNumber(), "214189");
     }
 
     @Test
