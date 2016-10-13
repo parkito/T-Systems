8754bc4158e59a6db11d01f3aa4f28998db6fdec
parkito <artyom-karnov@yandex.ru>
2016-10-13 21:33:28 +0300

    Finish line #17

--- eCare/OperatorMainApp/src/main/java/operator/utils/SearchResult.java
+++ eCare/OperatorMainApp/src/main/java/operator/utils/SearchResult.java
@@ -4,10 +4,12 @@ import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.Client;
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.elasticsearch.search.SearchHit;
 import org.springframework.stereotype.Service;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.Map;
 
 /**
  * Created by Artyom Karnov on 10/12/16.
@@ -16,10 +18,17 @@ import java.net.UnknownHostException;
 @Service("SearchResult")
 public class SearchResult {
     public String getResults(String str) throws UnknownHostException {
+        String result = "Nothing found";
         Client client = TransportClient.builder().build()
                 .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("localhost"), 9300));
         SearchResponse search = client.prepareSearch("operator").setTypes("tariffs").execute().actionGet();
         String serch = search.toString();
-        return serch;
+
+        for (SearchHit hit : search.getHits()) {
+            Map<String, Object> searchMap = hit.getSource();
+            if (searchMap.containsValue(str)) ;
+            result = searchMap.toString();
+        }
+        return result;
     }
 }
